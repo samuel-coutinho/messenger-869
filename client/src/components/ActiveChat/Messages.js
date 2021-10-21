7b2e0606a0d0abc439d9fca9f9efea28000176ff
samuel-coutinho <sfcrp2000@gmail.com>
2021-10-20 22:24:03 -0400

    Messages displayed in order with the oldest messages at the top and newest messages at the bottom.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
-import moment from "moment";
+import  moment  from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  
+  const sortedMessagesByCreatedAt = messages.slice().sort((a, b) => moment(a.createdAt) - moment(b.createdAt));
 
   return (
     <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format("h:mm");
+      { sortedMessagesByCreatedAt.map((message) => {
+        const time = moment(message.createdAt).format('MMM Do YY, h:mm:ss a');
 
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
