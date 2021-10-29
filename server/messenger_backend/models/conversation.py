2787e9b5100afc60f701bf4dc8c84cb654243e79
samuel-coutinho <sfcrp2000@gmail.com>
2021-10-28 23:38:28 -0400

    Conversation model changed

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -4,25 +4,23 @@ from django.db.models import Q
 from . import utils
 from .user import User
 
-
 class Conversation(utils.CustomModel):
 
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
-    )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
-    )
+    users = models.ManyToManyField(User)
+
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-    # find conversation given two user Ids
-    def find_conversation(user1Id, user2Id):
+    # find conversation given all users Ids
+    def find_conversation( list_of_participants ):
         # return conversation or None if it doesn't exist
         try:
-            return Conversation.objects.get(
-                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
-            )
+            participant = list_of_participants[0]
+            conversation = Conversation.objects.filter( users__contains = participant )
+            for participant in range(1, len(list_of_participants)):
+                conversation = conversation.filter( users__contains = participant )
+
+            return conversation          
         except Conversation.DoesNotExist:
             return None
+  
